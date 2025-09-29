02ded49d14e2ed047e994595e940ddd7046f703c
Latif <latif_1995@live.no>
2025-09-29 14:43:04 +0200

    typescript foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 
-import Zoo from "./data.tsx";
-
 function App() {
-  const [dyrList, setDyrList] = useState<Dyr[]>(() => [...Zoo.dyrene]);
-
-  /*
-  type Ansatt = {
-    navn: string;
-    alder: number;
-    ansattÅr: number;
-  };
-*/
-
-  type Dyr = {
-    navn: string;
-    høydeMeter: number;
-    favoritt: {
-      mat: string;
-      aktivitet: string;
-    };
-  };
-
-  const rev: Dyr = {
-    navn: "misterlure-lur",
-    høydeMeter: 1.25,
-    favoritt: {
-      mat: "hamster",
-      aktivitet: "jakte",
-    },
-  };
-
-  function leggTilDyr() {
-    setDyrList((prevList) => [...prevList, rev]);
+  function getFullName(firstName: string, lastName: string, alder?: number) {
+    return `mitt navn er ${firstName} ${lastName} ${!alder ? "" : alder}`;
   }
 
   return (
     <>
-      <main>
-        {" "}
-        han heter {rev.navn} og liker å spise {rev.favoritt.mat}
-        Det er totalt {Zoo.dyrene.length} i dyrehagen;
-        <button onClick={leggTilDyr}> legg til rev</button>
-        Det er nå totalt {dyrList.length} i dyrehagen. De heter{" "}
-        {dyrList.map((dyr) => (
-          <p key={dyr.navn}>{dyr.navn}</p>
-        ))}
-      </main>
+      <main>{getFullName("Latif", "Hassan")}</main>
     </>
   );
 }
